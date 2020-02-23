3c5046d583636d7e9beddebaaf7a1b1ad8ce9a8e
Shinkiyou <guilherme-fernandes80@hotmail.com>
2020-02-23 14:18:48 -0300

    Adds trash as dep

--- bin/index.js
+++ bin/index.js
@@ -3,9 +3,11 @@
 const fs = require("fs").promises
 const path = require("path")
 const argv = require("yargs").argv
+const trash = require("trash")
 
 const target = "./"
 const extensionFilter = argv._[0]
+const permanently = argv._[1] && argv._[1].includes("perm")
 
 async function main() {
     if(!extensionFilter) return console.log("Missing file extension. (purge-spell {extension})")
@@ -16,9 +18,16 @@ async function main() {
         const filePath = path.join(target, file)
         try {
             if(fileExtension === extensionFilter) {
-                await fs.unlink(filePath)
-                deleted++
-                console.log(`Deleted file ${file}...`)
+                if(permanently) {
+                    await fs.unlink(filePath)
+                    deleted++
+                    console.log(`Deleted file ${file}...`)
+                }
+                else {
+                    await trash(filePath)
+                    deleted++
+                    console.log(`Moved file ${file} to recycle bin...`)
+                }
             }
         }
         catch(error) {
